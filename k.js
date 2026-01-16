0032db5c77dc5db665a94de86682ffe8682fa30b
Nikhilkale2 <nikhilkaledev@gmail.com>
2026-01-16 14:07:31 +0530

    practice gsap, lenis and swiper JS

--- k.js
+++ k.js
@@ -19,3 +19,35 @@ button.addEventListener("click", function () {
     isclick = true;
   }
 });
+
+function swiperAnimation() {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    effect: "coverflow",
+    coverFlowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+      limitRotation: true,
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
+swiperAnimation();
